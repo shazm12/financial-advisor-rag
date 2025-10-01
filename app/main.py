872ee650510e55e9f7b563d3f6d9294eb2c410bf
Shamik Bera <berashamik115@gmail.com>
2025-10-01 18:41:02 +0530

    feat: Integrate session management into extract_pdf and query endpoints, enabling session-based retrieval of transaction and full text data

--- app/main.py
+++ app/main.py
@@ -10,14 +10,15 @@ from app.utils.retreiver import Retreiver
 from dotenv import load_dotenv
 import os
 
+from app.utils.session_manager import SessionManager
+
 load_dotenv()
 REDIS_URL = os.getenv("REDIS_URL")
 
 
-transactions_retreiver = Retreiver()
-full_text_retreiver = Retreiver()
 document_extractor = DocumentExtractor()
 embedding = CreateEmbeddings()
+session_manager = SessionManager()
 redis_db = RedisDB(redis_url=REDIS_URL)
 
 app = FastAPI(
@@ -50,20 +51,30 @@ async def extract_pdf():
     try:
         redis_db.ping()
         redis_db.flush_memory()
+        session_id = session_manager.create_sesssion()
         pdf_path = "/Users/shamikbera/Documents/Projects/finance-advisor-rag/app/data/Millennia Credit Card Statement Sept 2025.pdf"
         result = document_extractor.extract_statement(pdf_path)
         transactions_data = result["tables_data"]["transactions"]
         text_data = result["full_text"]
         trxn_rds = embedding.create_embeddings_for_transactions_data(
-            transactions_data=transactions_data
+            transactions_data=transactions_data, session_id=session_id
         )
         text_rds = embedding.create_embeddings_for_text_data(
-            text_data=text_data, index_name="full_text_data"
+            text_data=text_data, index_name="full_text_data", session_id=session_id
         )
-        transactions_retreiver.set_retreiver(trxn_rds)
-        full_text_retreiver.set_retreiver(text_rds)
+        transactions_retreiver = Retreiver(rds=trxn_rds)
+        full_text_retreiver = Retreiver(rds=text_rds)
+        session_manager.add_retreivers_to_session(
+            session_id,
+            {
+                "transactions_retreiver": transactions_retreiver,
+                "full_text_retreiver": full_text_retreiver,
+            },
+        )
         response = ExtractionResponse(
-            status=Status.SUCCESS, description="Extraction Successful"
+            status=Status.SUCCESS,
+            description="Extraction Successful",
+            session_id=session_id,
         )
         return response
     except Exception as e:
@@ -79,18 +90,37 @@ async def extract_pdf():
 @app.post("/query")
 async def query(request: QueryRequest):
     try:
+        session_id = request.session_id
+        if not session_manager.session_exists(session_id):
+            raise HTTPException(
+                status_code=400,
+                detail=QueryResponse(
+                    status=Status.FAILURE,
+                    response="",
+                    description="Session does not exist",
+                    session_id=session_id,
+                ).model_dump(mode="json"),
+            )
+        transactions_retreiver = session_manager.get_session_retreivers_by_id(
+            session_id
+        )["transactions_retreiver"]
+        full_text_retreiver = session_manager.get_session_retreivers_by_id(session_id)[
+            "full_text_retreiver"
+        ]
         query_chain = QueryChain(
             transactions_retriever=transactions_retreiver,
             full_text_retriever=full_text_retreiver,
         )
         answer = query_chain.invoke(request.prompt)
-        response = QueryResponse(status=Status.SUCCESS, response=answer)
+        response = QueryResponse(
+            status=Status.SUCCESS, session_id=session_id, response=answer
+        )
         return response
     except Exception as e:
         print(f"Error: {e}")
         raise HTTPException(
             status_code=400,
             detail=ExtractionResponse(
-                status=Status.FAILURE, description=f"{e}"
+                status=Status.FAILURE, description=f"{e}", session_id=session_id
             ).model_dump(mode="json"),
         )
--- app/models/request.py
+++ app/models/request.py
@@ -3,4 +3,5 @@ from pydantic import BaseModel
 
 
 class QueryRequest(BaseModel):
+    session_id: str
     prompt: str
