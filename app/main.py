6b05459265970e7fc03dd279c85aeaf452ece131
Shamik Bera <berashamik115@gmail.com>
2025-10-08 20:13:58 +0530

    refactor: Streamline main.py by removing unused imports and integrating extraction and query controllers for improved modularity

--- app/main.py
+++ app/main.py
@@ -1,25 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.models.request import QueryRequest
-from app.models.response import ExtractionResponse, QueryResponse, Status
-from app.utils.chains.query_chain import QueryChain
-from app.utils.create_embeddings import CreateEmbeddings
-from app.utils.document_extractor import DocumentExtractor
-from app.utils.redisdb import RedisDB
-from app.utils.retreiver import Retreiver
-from dotenv import load_dotenv
-import os
-
-from app.utils.session_manager import SessionManager
-
-load_dotenv()
-REDIS_URL = os.getenv("REDIS_URL")
-
-
-document_extractor = DocumentExtractor()
-embedding = CreateEmbeddings()
-session_manager = SessionManager()
-redis_db = RedisDB(redis_url=REDIS_URL)
+from app.controllers import extraction_controller, query_controller
 
 app = FastAPI(
     title="FastAPI Server",
@@ -36,6 +17,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(extraction_controller.router)
+app.include_router(query_controller.router)
 
 @app.get("/")
 async def root():
@@ -44,83 +27,3 @@ async def root():
         "message": "Welcome to FastAPI Server",
         "docs": "/docs",
     }
-
-
-@app.get("/extract", response_model=ExtractionResponse)
-async def extract_pdf():
-    try:
-        redis_db.ping()
-        redis_db.flush_memory()
-        session_id = session_manager.create_sesssion()
-        pdf_path = "/Users/shamikbera/Documents/Projects/finance-advisor-rag/app/data/Millennia Credit Card Statement Sept 2025.pdf"
-        result = document_extractor.extract_statement(pdf_path)
-        transactions_data = result["tables_data"]["transactions"]
-        text_data = result["full_text"]
-        trxn_rds = embedding.create_embeddings_for_transactions_data(
-            transactions_data=transactions_data, session_id=session_id
-        )
-        text_rds = embedding.create_embeddings_for_text_data(
-            text_data=text_data, index_name="full_text_data", session_id=session_id
-        )
-        transactions_retreiver = Retreiver(rds=trxn_rds)
-        full_text_retreiver = Retreiver(rds=text_rds)
-        session_manager.add_retreivers_to_session(
-            session_id,
-            {
-                "transactions_retreiver": transactions_retreiver,
-                "full_text_retreiver": full_text_retreiver,
-            },
-        )
-        response = ExtractionResponse(
-            status=Status.SUCCESS,
-            description="Extraction Successful",
-            session_id=session_id,
-        )
-        return response
-    except Exception as e:
-        print(f"Error: {e}")
-        raise HTTPException(
-            status_code=400,
-            detail=ExtractionResponse(
-                status=Status.FAILURE, description=f"{e}"
-            ).model_dump(mode="json"),
-        )
-
-
-@app.post("/query")
-async def query(request: QueryRequest):
-    try:
-        session_id = request.session_id
-        if not session_manager.session_exists(session_id):
-            raise HTTPException(
-                status_code=400,
-                detail=QueryResponse(
-                    status=Status.FAILURE,
-                    response="",
-                    description="Session does not exist",
-                    session_id=session_id,
-                ).model_dump(mode="json"),
-            )
-        transactions_retreiver = session_manager.get_session_retreivers_by_id(
-            session_id
-        )["transactions_retreiver"]
-        full_text_retreiver = session_manager.get_session_retreivers_by_id(session_id)[
-            "full_text_retreiver"
-        ]
-        query_chain = QueryChain(
-            transactions_retriever=transactions_retreiver,
-            full_text_retriever=full_text_retreiver,
-        )
-        answer = query_chain.invoke(request.prompt)
-        response = QueryResponse(
-            status=Status.SUCCESS, session_id=session_id, response=answer
-        )
-        return response
-    except Exception as e:
-        print(f"Error: {e}")
-        raise HTTPException(
-            status_code=400,
-            detail=ExtractionResponse(
-                status=Status.FAILURE, description=f"{e}", session_id=session_id
-            ).model_dump(mode="json"),
-        )
